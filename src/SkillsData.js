d9c8791940fdbd3a786ea6a7ce402e2300be92b3
Prajwal Tarpe <prajwaltarpe8@gmail.com>
2024-07-30 23:09:06 +0530

    done

--- src/SkillsData.js
+++ src/SkillsData.js
@@ -5,37 +5,37 @@ export const skillsData = [
         image: '/cpp.png'
     },
     {
-        id:3,
+        id:2,
         name: 'HTML',
         image: '/html.png'
     },
     {
-        id:4,
+        id:3,
         name: 'CSS',
         image: '/css.png'
     },
     {
-        id:5,
+        id:4,
         name: 'javascript',
         image: '/js.png'
     },
     {
-        id:6,
+        id:5,
         name: 'React',
         image: '/reactjs.png'
     },
     {
-        id:7,
+        id:6,
         name: 'Tailwind-css',
         image: '/tailwind-css-icon.png'
     },
     {
-        id:8,
+        id:7,
         name: 'SQL',
         image: '/SQL-logo.png'
     },
     {
-        id:9,
+        id:8,
         name: 'Git',
         image: '/Git-Icon.png'
     }
